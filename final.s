a816dd8b033e62e5de95c87e0bbc9cee7f84bdd7
RashaKhandelwal <69077950+RashaKhandelwal@users.noreply.github.com>
2020-10-01 15:49:12 +0530

    Update final.s

--- final.s
+++ final.s
@@ -6,11 +6,11 @@ __main  FUNCTION
 		MOV R7, #0x03
 		MOV R4, #0x04
 		CMP R2 , R7	
-		ITTE LT 
-		SUBLT R2, R2 , R7
-		MOVLT R2, #0x00
-		ADDGE R2, R2 ,R4
-		MOVGE R4, #0x00
+		ITTE NE 
+		SUBNE R2, R2 , R7
+		MOVNE R2, #0x00
+		ADDEQ R2, R2 ,R4
+		MOVEQ R4, #0x00
 stop    b stop		
      ENDFUNC
      END
